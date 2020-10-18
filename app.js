3d12c6ee22a2d350a2013ed3f6041c7d42d0190d
daisuke <dsaito85a@gmail.com>
2020-10-18 18:27:10 +0900

    モデルを追加

--- app.js
+++ app.js
@@ -38,4 +38,43 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// //Import the mongoose module
+// var mongoose = require('mongoose');
+
+// //Set up default mongoose connection
+// var mongoDB = 'mongodb+srv://daisuke:daisuke@cluster0.coftu.mongodb.net/daisuke?retryWrites=true&w=majority';
+// mongoose.connect(mongoDB);
+// // Get Mongoose to use the global promise library
+// mongoose.Promise = global.Promise;
+// //Get the default connection
+// var db = mongoose.connection;
+
+// //Bind connection to error event (to get notification of connection errors)
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://daisuke:daisuke@cluster0.coftu.mongodb.net/daisuke?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+var mongoose = require('mongoose');
+//Define a schema
+var Schema = mongoose.Schema;
+
+var SomeModelSchema = new Schema({
+  a_string: String,
+  a_date: Date
+});
+
+// Compile model from schema
+var SomeModel = mongoose.model('SomeModel', SomeModelSchema );
+
+
+
 module.exports = app;
